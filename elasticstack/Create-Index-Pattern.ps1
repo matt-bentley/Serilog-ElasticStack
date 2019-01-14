155cd0eb04b9f777dbf8cfd9ba591596b4b3fde9
Bentley, Matt (UK - London) <mabentley@deloitte.co.uk>
2019-01-14 18:46:47 +0000

    initial

---
+++ elasticstack/Create-Index-Pattern.ps1
@@ -0,0 +1,7 @@
+$Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$Headers.Add("Content-Type", "application/json")
+$Headers.Add("kbn-version", "6.5.4")
+Invoke-RestMethod "http://localhost:5601/api/saved_objects/index-pattern" `
+      -Method Post `
+      -Headers $Headers `
+      -Body '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}'
---
+++ serilog/SerilogPublisher/SerilogPublisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+using Serilog.Sinks.Http.BatchFormatters;
+using System;
+using System.Threading;
+
+namespace SerilogPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.Http(
+                    requestUri: "http://localhost:31312",
+                    batchFormatter: new ArrayBatchFormatter())
+                .WriteTo.Console()
+                .CreateLogger()
+                .ForContext<Program>();
+
+            logger.Information("Starting Serilog Publisher...");
+
+            for(int i = 0; i < 30; i++)
+            {
+                if(i % 10 == 0)
+                {
+                    try
+                    {
+                        throw new Exception($"There was an error processing {i}");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex, ex.Message);
+                    }
+                }
+                else
+                {
+                    logger.Information($"Processed cycle {i}");
+                }             
+                
+                Thread.Sleep(50);
+            }
+
+            logger.Information("Finishing Serilog Publisher");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
